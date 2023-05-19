9815042d1926c256e7b7495ac029846b6c2a8e40
Pessinaaa <pedro.pessina99@gmail.com>
2023-05-19 10:26:53 -0300

    CRUD em C# ASP.NET

---
+++ C#/Aula11/Aula11/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aula11
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Codigo { set; get; }
+        public String Nome { set; get; }
+        public String Email { set; get; }
+    }
+}
